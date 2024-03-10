82d11ed064e4591c66dc49321cb2917edbaa2956
Alok kumar yadav <alok953280@gmail.com>
2024-03-10 09:37:46 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Login from './components/Login';
 import Verify from './components/Verify';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OTP from './components/otp';
-import Marriage from './components/marriage';
 import Catering from './components/Catering';
-import BirthdayParty from './components/BirthdayParty';
-import Party from './components/Party';
-import PersonalStay from './components/personalStay';
-import Confrence from './components/Conference';
-import Anniversary from './components/anniversary';
 import Nav from './components/Navbar';
 import Footer from './components/Footer';
 import Requirement from './components/Requirement';
 import Hotels from './components/hotels';
-import Conference from './components/Conference';
 import Gatherings from './components/Gatherings';
 
 function App() {
@@ -24,25 +16,19 @@ function App() {
     <>
       {/* <Nav /> */}
       <Router>
-        <Nav/>
+        <Nav />
         <Routes>
-          {/* <Route path='/' element={} > </Route> */}
           <Route path='/' element={<Login />} > </Route>
           <Route path='/otp' element={<OTP />} > </Route>
           <Route path='/requirement' element={<Requirement />} > </Route>
           <Route path='/gathering' element={<Gatherings />} > </Route>
-          
-          {/* <Route path='/marriage' element={<Marriage />} > </Route>
-          <Route path='/birthdayParty' element={<BirthdayParty />} > </Route>
-          <Route path='/party' element={<Party />} > </Route>
-          <Route path='/personalStay' element={<PersonalStay />} > </Route>
-          <Route path='/conference' element={<Conference />} > </Route>
-          <Route path='/anniversary' element={<Anniversary />} > </Route>
-          <Route path='/verify' element={<Verify />} > </Route>
+
           <Route path='/catering' element={<Catering />} > </Route>
-          <Route path='/hotels' element={<Hotels />} > </Route> */}
+          <Route path='/verify' element={<Verify />} > </Route>
+          <Route path='/hotels' element={<Hotels />} > </Route>
+
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </>
   )
--- src/components/Gatherings.jsx
+++ src/components/Gatherings.jsx
@@ -69,7 +69,7 @@ const Gatherings = () => {
                                 <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={card.image} alt={card.title} />
                             </div>
                             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-                            <Link to={`/requirement?option=${option}`} className="absolute inset-0 flex translate-y-[38%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                            <Link to={`/catering`} className="absolute inset-0 flex translate-y-[38%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
                                 <h1 className="font-dmserif text-3xl font-bold text-white ">{card.title}</h1>
                                 {card.description && <h1 className="font-dmserif text-3xl font-bold text-white ">{card.description}</h1>}
                             </Link>
