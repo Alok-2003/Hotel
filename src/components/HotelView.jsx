ffe2d0477c2dc64b0fd72e4d6de42c1bccaf8318
Alok kumar yadav <alok953280@gmail.com>
2024-03-12 01:11:25 +0530

    done

--- src/components/HotelView.jsx
+++ src/components/HotelView.jsx
@@ -4,15 +4,15 @@ import ImageGrid from './ImageGrid';
 import { MdConnectingAirports } from "react-icons/md";
 import { TbAirConditioning } from "react-icons/tb";
 import { LuParkingCircle } from "react-icons/lu";
-import { FaHospital, FaWifi } from "react-icons/fa";
+import { FaHospital, FaTrain, FaWifi } from "react-icons/fa";
 import { MdRestaurant } from "react-icons/md";
 import { MdLocalBar } from "react-icons/md";
 
 const slides = [
-    "https://i.ibb.co/ncrXc2V/1.png",
-    "https://i.ibb.co/B3s7v4h/2.png",
-    "https://i.ibb.co/XXR8kzF/3.png",
-    "https://i.ibb.co/yg7BSdM/4.png"
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/b9/exterior.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/fb/the-grand-ballroom.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/f7/lava-bar.jpg?w=1200&h=-1&s=1",
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/f0/amenities.jpg?w=1200&h=-1&s=1"
 ];
 
 const HotelView = () => {
@@ -36,7 +36,7 @@ const HotelView = () => {
         { icon: <FaTrain />, name: 'New Delhi Hazrat Nizamuddin Station', distance: '10 min drive' },
         { icon: <FaTrain />, name: 'Delhi Aero City Station', distance: '18 min drive' },
         { icon: <FaTrain />, name: 'IGI Airport Station', distance: '21 min drive' }
-      ];
+    ];
 
     return (
         <div>
@@ -58,8 +58,8 @@ const HotelView = () => {
                     {/* Description */}
                     <div className="max-w-xl mx-auto bg-white p-4 rounded shadow-lg md:max-w-full md:m-2 md:flex md:justify-between">
                         <div className='md:'>
-                            <h2 className="text-xl font-bold mb-2 md:text-4xl">The Manor - New Delhi</h2>
-                            <p className="text-gray-700 mb-4 md:text-xl">Luxury New Delhi hotel with restaurant, connected to a shopping center</p>
+                            <h2 className="text-xl font-bold mb-2 md:text-4xl">Taj Chandigarh</h2>
+                            <p className="text-gray-700 mb-4 md:text-xl font-semibold">Luxury hotel with restaurant, connected to a shopping center</p>
 
                             <div className="flex items-center mb-3">
                                 <span className="bg-green-500 text-white p-1 rounded mr-2">9.0</span>
@@ -88,20 +88,8 @@ const HotelView = () => {
 
                         <div className='rounded-full'>
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3429.043813144539!2d76.78222047466204!3d30.745270785028293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a09098a47aa89%3A0xc0ef30e113006e91!2sTaj%20Chandigarh!5e0!3m2!1sen!2sin!4v1710182425959!5m2!1sen!2sin" width="330" height="250" allowfullscreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                        </div>
-
-                        <div className="max-w-xs mx-auto bg-white p-4 rounded shadow-lg">
-                            <h2 className="text-xl font-bold mb-2">The Manor - New Delhi</h2>
-                            <p className="text-gray-700 mb-4">Luxury New Delhi hotel with restaurant, connected to a shopping center</p>
-
-                            <div className="flex items-center mb-3">
-                                <span className="bg-green-500 text-white p-1 rounded mr-2">9.0</span>
-                                <span>Wonderful</span>
-                            </div>
-
-                            <a href="#" className="text-blue-500 text-sm mb-4 inline-block">See all 101 reviews &gt;</a>
-
-                            <ul className="mb-4">
+                            <p className="text-gray-700 my-2 text-xl font-bold"> Check out the area </p>
+                            <ul className="mb-2">
                                 {locations.map((location, index) => (
                                     <li key={index} className="flex items-center mb-1">
                                         {location.icon}
@@ -112,8 +100,7 @@ const HotelView = () => {
                                     </li>
                                 ))}
                             </ul>
-
-                            <button className="text-blue-500 text-sm">See more &gt;</button>
+                            <button className="text-blue-500 ">See more &gt;</button>
                         </div>
                     </div>
                 </div>
--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -5,14 +5,14 @@ const ImageGrid = () => {
     <div className="grid grid-cols-2 gap-1 ">
       {/* Big Image */}
       <div>
-        <img src="https://i.ibb.co/ncrXc2V/1.png" alt="Big" className="w-full  rounded-l-3xl " />
+        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/b9/exterior.jpg?w=1200&h=-1&s=1" alt="Big" className="w-full  rounded-l-3xl " />
       </div>
       {/* Small Images */}
       <div className="grid grid-cols-2 gap-1  p-[0.75px]">
-        <img src="https://i.ibb.co/B3s7v4h/2.png" alt="Small 1" className="" />
-        <img src="https://i.ibb.co/B3s7v4h/2.png" alt="Small 1" className=" rounded-tr-3xl" />
-        <img src="https://i.ibb.co/XXR8kzF/3.png" alt="Small 2" className=" " />
-        <img src="https://i.ibb.co/XXR8kzF/3.png" alt="Small 2" className=" rounded-br-3xl" />
+        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/fb/the-grand-ballroom.jpg?w=1200&h=-1&s=1" alt="Small 1" className="w-full h-full" />
+        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/f9/lava-bar.jpg?w=1200&h=-1&s=1" alt="Small 1" className=" rounded-tr-3xl h-full" />
+        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/f7/lava-bar.jpg?w=1200&h=-1&s=1" alt="Small 2" className="h-full " />
+        <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/07/d7/f0/amenities.jpg?w=1200&h=-1&s=1" alt="Small 2" className=" rounded-br-3xl h-full"  />
       </div>
     </div>
   );
