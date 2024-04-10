7c1f8460b6cd4e6f089319ae2bbc813c15621563
Alok kumar yadav <alok953280@gmail.com>
2024-04-10 08:36:02 +0530

    done

--- src/components/Catering.jsx
+++ src/components/Catering.jsx
@@ -1,55 +1,58 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Select from 'react-select';
+import { useNavigate } from "react-router-dom";
 import '../App.css'
 
+export let selectedCateringGlobal = '';
 
 const Catering = () => {
+    const [catering, setCatering] = useState({ value: null, label: 'Choose Option' });
+    const navigate = useNavigate();
 
-    const [Catering, setCatering] = useState({ value: null, label: 'Choose Option' });
-    const CateringOptions = [
+
+    const cateringOptions = [
         { value: 'Yes', label: 'Yes' },
-        { value: "No", label: "No" },
-
+        { value: 'No', label: 'No' },
     ];
+
     const handleCatering = (selectedOption) => {
-        setRequirement(selectedOption);
+        setCatering(selectedOption);
+        selectedCateringGlobal = selectedOption.value;
+        navigate("/hotels");
+
     };
+console.log(selectedCateringGlobal);
     return (
         <>
             <div className='flex justify-center items-center bg-[url("src/assets/1.jpg")] bg-cover'>
                 <div className='md:min-h-[100vh]  flex justify-center items-center w-5/6  '>
                     <div className=''>
-
-                    <div className="flex justify-center">
-                        <div className="flex font-bold text-white md:text-5xl text-2xl leading-8  h-6  md:mb-12 ">Do you want catering in house</div>
-                    </div>
-                    <div className="h-[60vh] grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2 ">
-                        {/* 1st */}
-                        <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
-                            <div className="h-[60vh] bg-teal-600 ">
-                                <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="/Catering.jpg" alt="" />
-                            </div>
-                            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent group-hover:from-black/30 group-hover:via-black/30 group-hover:to-black/30"></div>
-                            <Link to='/hotels' className="absolute inset-0 flex md:translate-y-[38%] translate-y-[34%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-                                <h1 className="font-dmserif md:text-6xl text-4xl font-bold text-black textStroke">Yes</h1>
-                                {/* <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p> */}
-                                {/* <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button> */}
-                            </Link>
+                        <div className="flex justify-center">
+                            <div className="flex font-bold text-white md:text-5xl text-2xl leading-8  h-6  md:mb-12 ">Do you want catering in house</div>
                         </div>
-                        {/* 2nd */}
-                        <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
-                            <div className="h-[60vh] bg-red-600 ">
-                                <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 opacity-50" src="/Catering.jpg" alt="" />
+                        <div className="h-[60vh] grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2 ">
+                            {/* 1st */}
+                            <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                                <div className="h-[60vh] bg-teal-600 ">
+                                    <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="/Catering.jpg" alt="" />
+                                </div>
+                                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent group-hover:from-black/30 group-hover:via-black/30 group-hover:to-black/30"></div>
+                                <div className="absolute inset-0 flex md:translate-y-[38%] translate-y-[34%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0" onClick={() => handleCatering(cateringOptions[0])}>
+                                    <h1 className="font-dmserif md:text-6xl text-4xl font-bold text-black textStroke">Yes</h1>
+                                </div>
                             </div>
-                            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent group-hover:from-black/30 group-hover:via-black/30 group-hover:to-black/30"></div>
-                            <Link to='/hotels' className="absolute inset-0 flex md:translate-y-[38%] translate-y-[34%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-                                <h1 className="font-dmserif md:text-6xl text-4xl font-bold text-black textStroke">No</h1>
-                                {/* <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p> */}
-                                {/* <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button> */}
-                            </Link>
+                            {/* 2nd */}
+                            <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                                <div className="h-[60vh] bg-red-600 ">
+                                    <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 opacity-50" src="/Catering.jpg" alt="" />
+                                </div>
+                                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent group-hover:from-black/30 group-hover:via-black/30 group-hover:to-black/30"></div>
+                                <div className="absolute inset-0 flex md:translate-y-[38%] translate-y-[34%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0" onClick={() => handleCatering(cateringOptions[1])}>
+                                    <h1 className="font-dmserif md:text-6xl text-4xl font-bold text-black textStroke">No</h1>
+                                </div>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -1,37 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from './Footer';
 import FooterLp from './FooterLp';
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { useFirebase } from '../context/Firebase';
+import { selectedCityGlobal } from './HSearch';
+import { selectedCardGlobal } from './Requirement';
+import { selectedGatheringGlobal } from './Gatherings';
+import { selectedCateringGlobal } from './Catering';
 
 const Hotels = () => {
-    // Array of hotel objects
-    const Typehotels = [
-        { name: "JW Marriott", location: "Location", imageUrl: "https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg?t=st=1709113991~exp=1709117591~hmac=df8851ee7280dc92a328d1fe2e7b9aa479849fe274fa241cbc7afd4290bcb21a&w=740" },
-        { name: "Vivanta by Taj", location: "Location", imageUrl: "https://img.freepik.com/premium-photo/high-end-clean-atmospheric-hotel-rooms_149197-85.jpg?w=740" },
-        { name: "The Leela Palace", location: "Location", imageUrl: "https://img.freepik.com/free-photo/sunset-pool_1203-3192.jpg?t=st=1710364047~exp=1710367647~hmac=6fdf83eae64bd7de55411b3c8d80ce4273eb3e82d0bfd224b923e54573fcc376&w=740" },
-        { name: "ITC Grand Chola", location: "Location", imageUrl: "https://img.freepik.com/free-photo/luxury-bedroom-hotel_1150-10836.jpg?w=740" },
-        { name: "The Oberoi Amarvilas", location: "Location", imageUrl: "https://img.freepik.com/free-photo/swimming-pool_74190-1977.jpg?t=st=1710364204~exp=1710367804~hmac=2385fcab364a56829f2340952a66eb71138bd441f756002945b4ec59dd2f9433&w=740" },
-        { name: "The Oberoi Udaivilas", location: "Location", imageUrl: "https://img.freepik.com/free-photo/urban-city-architecture_649448-5343.jpg?t=st=1710364216~exp=1710367816~hmac=46b5d3f42c3964889a84e3ec8340a20b9379013a3792b67f49318fe416ec1a63&w=740" },
-    ];
+    const firebase = useFirebase();
+    const navigate = useNavigate();
+    const [hotels, sethotel] = useState([]);
 
+    useEffect(() => {
+        firebase.listOfHotels().then((hotels) => sethotel(hotels.docs.map(doc => doc.data())));
+    }, []);
+
+    const filterHotels = (hotels, city, card, gathering, catering) => {
+        return hotels.filter(hotel => {
+            const isSelectedCity = !city || hotel.location === city;
+            const isSelectedCard = !card || hotel.event === card;
+            const isSelectedGathering = !gathering || hotel.Strength === gathering;
+            const isSelectedCatering = !catering || hotel.meal === catering;
+            return isSelectedCity && isSelectedCard && isSelectedGathering && isSelectedCatering;
+        });
+    };
+    
+    const filteredHotels = filterHotels(hotels, selectedCityGlobal, selectedCardGlobal, selectedGatheringGlobal, selectedCateringGlobal);
+
+    console.log(hotels, selectedCityGlobal, selectedCardGlobal, selectedGatheringGlobal, selectedCateringGlobal)
     return (
-        <div className='h-full  font-[gilroy] bg-[url("src/assets/building-night.jpg")] bg-cover flex items-center'>
-
-            <div className='mx-12 backdrop-blur-sm bg-white/50 p-4  rounded-3xl'>
-                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 ">
-                    {Typehotels.map((hotel, index) => (
-                        <a href='/hotelView' key={index} className="relative rounded-3xl">
-                            <img src={hotel.imageUrl} alt="Image" className="w-full h-auto rounded-3xl" />
-                            <div className="flex justify-between items-end">
-                                <p className="text-2xl font-bold">{hotel.name}</p>
-                                <button className="text-xl mx-2 ">{hotel.location}</button>
-                            </div>
-                        </a>
-                    ))}
+        <div className='h-full font-[gilroy] bg-[url("src/assets/building-night.jpg")] bg-cover flex justify-center '>
+            <div className='w-10/12   mt-20 '>
+                <div className=" backdrop-blur-sm bg-white/50 rounded-3xl font-bold text-2xl  h-12 grid grid-cols-6 gap-4 justify-items-center items-center px-4">
+                    {/* Image */}
+                    <h1> Hotel Name </h1>
+                    <h1> Event:</h1>
+                    <h1> Catering:</h1>
+                    <h1> Location:</h1>
+                    <h1> Contact:</h1>
+                    <Link to={"/admin_create"}>
+                        <button className='bg-blue-500  px-2 py-1 rounded-xl' >Create</button>
+                    </Link>
                 </div>
+                {filteredHotels.map((h, index) => (
+                    <main key={index} className="   justify-center">
+                        <div className=" backdrop-blur-sm bg-white/50 rounded-3xl text-xl my-4 h-16 grid grid-cols-6 gap-4 justify-items-center items-center px-4">
+                            {/* Image */}
+                            <h1> {h.name} </h1>
+                            <h1> {h.event} </h1>
+                            <h1> {h.meal} </h1>
+                            <h1> {h.location} </h1>
+                            <h1> {h.contact}</h1>
+                            <button className='bg-teal-400   px-4 py-2 rounded-xl' >Edit</button>
+                        </div>
+                    </main>
+                ))}
             </div>
-            {/* Floating Interested button */}
-            
         </div>
     );
 };
