33c3675d2d7e0336c81c8fc4e82beabee38148bd
Alok kumar yadav <alok953280@gmail.com>
2024-05-20 09:33:23 +0530

    done

--- src/components/CliProfile.jsx
+++ src/components/CliProfile.jsx
@@ -12,7 +12,8 @@ const CliProfile = () => {
         firebase.listOfClient().then((profiles) => {
             setprofile(profiles.docs.map(doc => doc.data()))
         }
-    )}, []);
+        )
+    }, []);
     console.log(profiles)
 
     const creatorContacts = profiles.map(profile => profile.CreatorContact);
@@ -21,9 +22,9 @@ const CliProfile = () => {
 
     return (
         <div className='h-full font-[gilroy] bg-[url("src/assets/building-night.jpg")] bg-cover flex justify-center '>
-            <div className='w-10/12   mt-20 '>
-                <div className=" backdrop-blur-sm bg-white/50 rounded-3xl font-bold text-2xl  h-12 grid grid-cols-5 gap-4 justify-items-center items-center px-4">
-                    {/* Image */} 
+            <div className='lg:w-11/12  w-[95%]  mt-20 '>
+                <div className="hidden  backdrop-blur-sm bg-white/50 rounded-3xl font-bold text-sm lg:text-2xl  h-12 lg:grid lg:grid-cols-5 gap-4 justify-items-center items-center px-4">
+                    {/* Image */}
                     <h1> Client Name </h1>
                     <h1> City</h1>
                     <h1> Pincode</h1>
@@ -33,13 +34,15 @@ const CliProfile = () => {
                 {profiles.map((p, index) => (
 
                     <main key={index} className="   justify-center">
-                        <div className=" backdrop-blur-sm bg-white/50 rounded-3xl text-xl my-4 h-16 grid grid-cols-5 gap-4 justify-items-center items-center px-4">
-                            {/* Image */}
-                            <h1> {p.name} </h1>
-                            <h1> {p.city} </h1>
-                            <h1> {p.pincode} </h1>
-                            <h1> {p.email} </h1>
-                            <h1> {p.contact}</h1>
+                        {/* Image */}
+                        <div className=" backdrop-blur-sm bg-white/50 rounded-3xl  text-sm lg:text-[1.4rem]  my-4 py-4 h-full lg:h-16  lg:flex gap-4 justify-items-center items-center px-4">
+                            <h1 className='text-centre lg:ml-14 w-48 font-extrabold lg:font-thin text-2xl lg:text-[1.4rem]' > {p.name} </h1>
+                            <div className="grid grid-cols-2 lg:ml-4 gap-x-28  lg:text-center text-[1.1rem] lg:text-[1.4rem] lg:grid-cols-4 lg:gap-0 w-full">
+                                <h1> {p.city} </h1>
+                                <h1> {p.pincode} </h1>
+                                <h1 className=' truncate w-52 lg:w-full' > {p.email} </h1>
+                                <h1> {p.contact}</h1>
+                            </div>
                         </div>
                     </main>
                 ))}
--- src/components/CreateProfile.jsx
+++ src/components/CreateProfile.jsx
@@ -41,19 +41,19 @@ const CreateProfile = () => {
         <div className="min-h-screen flex items-center justify-center bg-gray-100 bg-[url('https://firebasestorage.googleapis.com/v0/b/hotel-60204.appspot.com/o/Background_Images%2FBG_2.jpg?alt=media&token=0a342de3-713c-4e07-8a6f-0645aa3e7eb8')] bg-cover">
             <Toaster toastOptions={{ duration: 4000 }} />
 
-            <div className="backdrop-blur-sm bg-white/30 p-8 rounded-3xl shadow-lg w-1/3">
+            <div className="backdrop-blur-sm bg-white/30 p-8 rounded-3xl shadow-lg w-[90%] lg:w-1/3">
                 <h2 className="text-4xl font-bold mb-2 text-black">Create Profile</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="fullName" className="block text-lg font-medium text-white">Full Name</label>
                         <input type="text" id="fullName" name="fullName" value={formData.fullName} onChange={handleChange} placeholder="Enter Full Name" className="mt-1 p-2 border border-gray-300 rounded-md w-full text-lg" />
                     </div>
-                    <div className='flex justify-between'>
-                        <div className="mb-4 w-5/12">
+                    <div className='flex justify-between '>
+                        <div className="mb-4 w-[48%] ">
                             <label htmlFor="city" className="block text-lg font-medium text-white">City</label>
                             <input type="text" id="city" name="city" value={formData.city} onChange={handleChange} placeholder="Enter City" className="mt-1 p-2 border border-gray-300 rounded-md w-full text-lg" />
                         </div>
-                        <div className="mb-4 w-5/12">
+                        <div className="mb-4 w-[48%]">
                             <label htmlFor="pincode" className="block text-lg font-medium text-white">Pincode</label>
                             <input type="number" id="pincode" name="pincode" value={formData.pincode} onChange={handleChange} placeholder="Enter Pincode" className="mt-1 p-2 border border-gray-300 rounded-md w-full text-lg" />
                         </div>
